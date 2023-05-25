bfac1582c3d730a26639fb7e78cbf341ff41d6e7
Asindu De Silva <asindudesilva@gmail.com>
2023-05-25 17:12:58 +0530

    created alert dropdown

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-var isDark=true;
-
-
 $(".theme-changer-btn").click(function(){
     $("body").toggleClass("light-theme");
+    //animations();
 });
+
+
+
+const tl = gsap.timeline({defaults: {duration: 0.7, ease: "power1.out"}})
+
+
+animations=()=>{
+    tl.fromTo('.location-name', {opacity: 0, y:-100}, {opacity: 1, y:0})
+    tl.fromTo('.current-outer', {opacity: 0, y:-100}, {opacity: 1, y:0})
+    
+}
